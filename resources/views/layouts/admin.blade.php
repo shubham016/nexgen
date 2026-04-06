2249c9260070d033e49140a12e60ac64d1d66709
Shubham <web.shubham1@gmail.com>
2026-04-06 23:40:06 +0545

    update admin favicon to nexgen_logo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8" />
     <title>@yield('title', 'NEXGEN Admin - Inventory Dashboard')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('admin-assets/images/logo.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('admin-assets/images/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('admin-assets/images/favicon-16x16.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/nexgen_logo.png') }}?v=2">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/nexgen_logo.png') }}?v=2">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/nexgen_logo.png') }}?v=2">
     <link rel="manifest" href="{{ asset('admin-assets/site.webmanifest') }}">
 
     <link rel="stylesheet" href="{{ asset('admin-assets/css/main.css') }}">
